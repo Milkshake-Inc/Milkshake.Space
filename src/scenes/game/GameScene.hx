ad28ce20dd1cb79afd2e7e31a398f16d4ac5702a
Lucas Jones <Necxii@gmail.com>
2014-05-21 23:16:05 +0100

    Added FlatUI

--- src/scenes/game/GameScene.hx
+++ src/scenes/game/GameScene.hx
@@ -4,7 +4,6 @@ import milkshake.game.scene.Scene;
 import milkshake.game.scene.SceneManager;
 import milkshake.IGameCore;
 
-
 class GameScene extends Scene
 {
 	public function new(core:IGameCore, sceneManager:SceneManager)
